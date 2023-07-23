0a6e7a7c2a9d99bd467bfe7aa705bdb9616028e2
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-07-23 20:20:10 +0000

    New structure set

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,10 +5,16 @@ import TodoList from "./TodoList.jsx";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<todoList/>
-		</div>
+		<form className="inputs">
+			<h1>Deucalinos weird to Dos</h1>
+			<div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+              <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+            </div>
+
+		</form>
 	);
+	console.log(Home);
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,13 +8,7 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 import TodoList from "./component/TodoList";
-const todoList= ()=>{
-    const {newTask,setNewTask}=useState('');
-    const {list, setList}=useState ([]);
-    function addNewTask() {
-          setList(list.concat(newTask));
-  }
-}
+
 
 //render your react application
-ReactDOM.render(<TodoList/>, document.querySelector("#app"));
+ReactDOM.render(<Home/>, document.querySelector("#app"));
